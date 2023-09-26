e8fbd1a0ff9ff0c24e70c7e9607e45f5b2376326
Caê Moreira <Ca Moreira@DESKTOP-DB79ACC>
2023-09-26 11:58:19 -0300

    Adicionar arquivos de projeto.

---
+++ dev-backend1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dev_backend1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
